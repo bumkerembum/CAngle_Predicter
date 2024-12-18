703a0ab01575f185be0d4b5174f075744b4ff16f
Kerem Dülger <124495822+bumkerembum@users.noreply.github.com>
2024-12-18 09:15:19 +0300

    Update resnet_initial_training.py
    Added comments for clearance

--- resnet_initial_training.py
+++ resnet_initial_training.py
@@ -6,13 +6,14 @@ from tensorflow.keras.optimizers import Adam
 import numpy as np
 from tensorflow.keras.applications.resnet50 import preprocess_input
 
+# This will disable GPU and allow CPU computing. If your CUDNN version is not compatible, you will get an error. Update your CUDNN or switch to CPU calculation.
 tf.config.set_visible_devices([], 'GPU')
 
 
 # Load pre-trained ResNet50 without the top layer
 base_model = ResNet50(weights='imagenet', include_top=False, input_tensor=Input(shape=(224, 224, 3)))
 
-# Add custom regression layers
+# Add custom top layers for regression
 x = base_model.output
 x = GlobalAveragePooling2D()(x)  # Pool features
 x = Dense(512, activation='relu')(x)  # Fully connected layer
@@ -22,8 +23,6 @@ output = Dense(1, activation='linear')(x)  # Linear activation for regression
 # Create the model
 model = Model(inputs=base_model.input, outputs=output)
 
-#
-
 # Freeze all ResNet50 layers
 for layer in base_model.layers:
     layer.trainable = False
@@ -31,18 +30,17 @@ for layer in base_model.layers:
 # Compile the model
 model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error', metrics=['mae'])
 
-X_train = np.load("/home/kerem/Desktop/contakbizim/data_matrix/X_train.npy")
-y_train = np.load("/home/kerem/Desktop/contakbizim/data_matrix/y_train.npy")
-X_val = np.load("/home/kerem/Desktop/contakbizim/data_matrix/X_val.npy")
-y_val = np.load("/home/kerem/Desktop/contakbizim/data_matrix/y_val.npy")
+X_train = np.load("training input location")
+y_train = np.load("training output location")
+X_val = np.load("validation input location")
+y_val = np.load("validation output location")
 
 
 history = model.fit(
     X_train, y_train,
     validation_data=(X_val, y_val),
-    epochs=100,  # Start with 10 epochs and adjust based on performance
-    batch_size=256,
+    epochs=100,     # Can be adjusted 
+    batch_size=256, # Can be adjusted
     #shuffle=True
 )
-model.save("/home/kerem/Desktop/contakbizim/saved_initial_training/100epoch_256batch.h5")
-#model.fit()
+model.save("folder location/model_name.h5")
