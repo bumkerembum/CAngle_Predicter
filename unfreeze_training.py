6d33904b6a4fd8ec84bcf5d3a65046376f0847c4
Kerem Dülger <124495822+bumkerembum@users.noreply.github.com>
2024-12-26 15:18:30 +0300

    Update unfreeze_training.py

--- unfreeze_training.py
+++ unfreeze_training.py
@@ -1,22 +1,18 @@
 import tensorflow as tf
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Input
-from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 import numpy as np
-from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.models import load_model
 
-tf.config.set_visible_devices([], 'GPU')
+tf.config.set_visible_devices([], 'GPU') #Disable thiss option activate GPU computing
 
 
 # Load initialliy tirained model and data sets
-model = load_model('/home/kerem/Desktop/contakbizim/saved_initial_training/100epoch_64batch.h5')
+model = load_model("pre_trained model's location")
 
-X_train = np.load("/home/kerem/Desktop/contakbizim/data_matrix/X_train.npy")
-y_train = np.load("/home/kerem/Desktop/contakbizim/data_matrix/y_train.npy")
-X_val = np.load("/home/kerem/Desktop/contakbizim/data_matrix/X_val.npy")
-y_val = np.load("/home/kerem/Desktop/contakbizim/data_matrix/y_val.npy")
+X_train = np.load("x_training_location")
+y_train = np.load("y_training_location")
+X_val = np.load("x_validation_locaiton")
+y_val = np.load("y_validation_location")
 
 # Set which layers to train
 for layer in model.layers:
@@ -30,9 +26,9 @@ model.compile(optimizer=Adam(learning_rate=1e-5),
 history_fine = model.fit(
     X_train, y_train,
     validation_data=(X_val, y_val),
-    epochs=200,  # Fine-tuning usually requires more epochs
-    batch_size=32
+    epochs=200,   # Adjust based on your preferance
+    batch_size=32 # Adjust based on your preferance
 )
 
-model.save("/home/kerem/Desktop/contakbizim/unfreeze_training_two/100epoch_64batch/200epoch_32batch.h5")
+model.save("location/model.h5")
     
